e62f44bbdd940fe82c85270bd3dddc8c203dfd67
Maria <mimito1353@gmail.com>
2023-08-05 11:43:14 +0300

    Update Login component to redirect

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,36 @@
-const Login = () => {
+const Login = ({
+  history
+}) => {
+
+  const onFormSubmit = (e) => {
+      e.preventDefault();
+      history.push('/games')
+  };
   return(
     <section id="login-page" className="auth">
-    <form id="login">
-      <div className="container">
-        <div className="brand-logo"></div>
-        <h1>Login</h1>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Sokka@gmail.com"
-        />
+      <form id="login" onSubmit={onFormSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Login</h1>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Sokka@gmail.com"
+          />
 
-        <label htmlFor="login-pass">Password:</label>
-        <input type="password" id="login-password" name="password" />
-        <input type="submit" className="btn submit" value="Login" />
-        <p className="field">
-          <span>
-            If you don't have profile click <a href="#">here</a>
-          </span>
-        </p>
-      </div>
-    </form>
-  </section>
+          <label htmlFor="login-pass">Password:</label>
+          <input type="password" id="login-password" name="password" />
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
+            <span>
+              If you don't have profile click <a href="#">here</a>
+            </span>
+          </p>
+        </div>
+      </form>
+    </section>
   );
 }
 
